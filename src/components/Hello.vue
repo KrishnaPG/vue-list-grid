f08ecd4517107d311e7ac8db12cc050bdce6e380
Gopalakrishna Palem <KrishnaPG@Yahoo.com>
2017-05-23 17:56:00 +0530

    working on integrating custom list and grid views

---
+++ src/components/Hello.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <listGrid :data=listGridData></listGrid>
+  </div>
+</template>
+
+<script>
+import listGrid from './listGrid'
+
+export default {
+  name: 'hello',
+  components: {
+    listGrid
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      listGridData: [{
+        title: 'Tapping into UGC with Offerpop',
+        url: 'https://voltagead.com/tapping-ugc-offerpop/',
+        image: {
+          'large': 'https://2e64oz2sjk733hqp882l9xbo-wpengine.netdna-ssl.com/wp-content/uploads/2016/08/header-960x500-copy-960x500.jpg',
+          'small': 'https://2e64oz2sjk733hqp882l9xbo-wpengine.netdna-ssl.com/wp-content/uploads/2016/08/header-960x500-copy-300x300.jpg'
+        }
+      }, {
+        title: '5 websites that get design right',
+        url: 'https://voltagead.com/5-websites-get-design-right/',
+        image: {
+          'large': 'https://2e64oz2sjk733hqp882l9xbo-wpengine.netdna-ssl.com/wp-content/uploads/2016/08/HERO-960x500.jpg',
+          'small': 'https://2e64oz2sjk733hqp882l9xbo-wpengine.netdna-ssl.com/wp-content/uploads/2016/08/HERO-300x300.jpg'
+        }
+      }, {
+        title: 'Mariachis, Hats, and Pies, Oh My!',
+        url: 'https://voltagead.com/mariachis-hats-pies-oh/',
+        image: {
+          'large': 'https://2e64oz2sjk733hqp882l9xbo-wpengine.netdna-ssl.com/wp-content/uploads/2016/07/IMG_2629.2-960x582.jpg',
+          'small': 'https://2e64oz2sjk733hqp882l9xbo-wpengine.netdna-ssl.com/wp-content/uploads/2016/07/IMG_2629.2-300x300.jpg'
+        }
+      }, {
+        title: 'Big buzz. Big brands. Reebok does omnichannel.',
+        url: 'https://voltagead.com/big-buzz-big-brands-reebok-omnichannel/',
+        image: {
+          'large': 'https://2e64oz2sjk733hqp882l9xbo-wpengine.netdna-ssl.com/wp-content/uploads/2016/07/reebok-hero2.jpg',
+          'small': 'https://2e64oz2sjk733hqp882l9xbo-wpengine.netdna-ssl.com/wp-content/uploads/2016/07/reebok-hero2-300x300.jpg'
+        }
+      }, {
+        title: 'Colorado Ad Day',
+        url: 'https://voltagead.com/denver-ad-day/',
+        image: {
+          'large': 'https://2e64oz2sjk733hqp882l9xbo-wpengine.netdna-ssl.com/wp-content/uploads/2016/07/sample-blog-960x577.jpg',
+          'small': 'https://2e64oz2sjk733hqp882l9xbo-wpengine.netdna-ssl.com/wp-content/uploads/2016/07/sample-blog-300x300.jpg'
+        }
+      }, {
+        title: 'Using the Ordinary to Build the Extraordinary',
+        url: 'https://voltagead.com/using-ordinary-build-extraordinary/',
+        image: {
+          'large': 'https://2e64oz2sjk733hqp882l9xbo-wpengine.netdna-ssl.com/wp-content/uploads/2016/06/header-960x500.jpg',
+          'small': 'https://2e64oz2sjk733hqp882l9xbo-wpengine.netdna-ssl.com/wp-content/uploads/2016/06/header-300x300.jpg'
+        }
+      }]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/listGrid.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="listGrid">
+		<div class="listGrid-toolbar"></div>
+		<div class="listGrid-body">
+			<div v-if="view=='list'" class="listGrid-listview">
+				<ul>
+					<li v-for="dataItem in data">{{dataItem.title}}, {{dataItem.url}}</li>
+				</ul>
+			</div>
+			<div v-if="view=='grid'" class="listGrid-gridView">
+				grid component
+			</div>
+		</div>		
+	</div>
+</template>
+<script>
+	export default {
+		name: 'listGrid',
+		props: {
+			'view': { type: String, default: 'list' },
+			'data': { default: () => {}	}
+		},
+		data: function() {
+			return {
+			}
+		},
+	}
+</script>
